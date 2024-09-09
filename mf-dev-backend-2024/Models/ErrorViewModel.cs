4e239850a9b16b2b523da8080b203c368461d812
FarleyDeSouza <nabucodonosor.ia.2023@gmail.com>
2024-09-09 20:53:18 -0300

    Adicionar arquivos de projeto.

---
+++ mf-dev-backend-2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mf_dev_backend_2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
